ae3d9b8bc45d027bc466b782efd03620bbf0428c
Otajon <otajonbozorboyev571@gamil.com>
2024-03-14 11:57:09 +0500

    first commit

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,9 +1,14 @@
 from aiogram import types
 
 from loader import dp
+import wikipedia
+wikipedia.set_lang('uz')
 
-
-# Echo bot
-@dp.message_handler(state=None)
-async def bot_echo(message: types.Message):
-    await message.answer(message.text)
+# Wikihandler
+@dp.message_handler()
+async def sendWiki(message: types.Message):
+    try:
+        respond = wikipedia.summary(message.text)
+        await message.answer(respond)
+    except:
+        await message.answer("Bu mavzuga oid maqola topilmadi")
---
+++ keyboards/inline/manage_post.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
+
+post_callback = CallbackData("create_post", "action")
+confirmation_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[[
+        InlineKeyboardButton(text="🖨 Chop etish", callback_data=post_callback.new(action='post')),
+        InlineKeyboardButton(text="❌ Rad etish", callback_data=post_callback.new(action='cancel'))
+    ]]
+)
+
+
+
